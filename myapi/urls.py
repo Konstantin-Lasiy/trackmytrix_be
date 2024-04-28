98d4b2d03c5647c64fd8d59718a9aca0dc6e2b4f
COsta <kostyalasiy@gmail.com>
2024-04-28 08:53:31 -0600

    added class lists

--- myapi/urls.py
+++ myapi/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
+
 from . import views
 
 app_name = "myapi"
 urlpatterns = [
     path("hello-world/", views.hello_world, name="hello_world"),
     path("upload_tricks/", views.upload_run, name="upload_tricks"),
-    path("trick_definitions/", views.get_trick_definitions, name="trick_definitions"),
+    path(
+        "trick_definitions/",
+        views.TrickDefinitionList.as_view(),
+        name="trick_definitions",
+    ),
+    path("runs/", views.RunList.as_view(), name="runs"),
 ]
--- myapi/views.py
+++ myapi/views.py
@@ -1,8 +1,8 @@
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework import status, permissions
+from rest_framework import status, permissions, generics
 from rest_framework.response import Response
 from .models import TrickInstance, Run, TrickDefinition
-from .serializers import TrickDefinitionSerializer
+from .serializers import TrickDefinitionSerializer, RunSerializer
 from django.http import JsonResponse
 from django.utils import timezone
 
@@ -11,6 +11,18 @@ from django.utils import timezone
 def hello_world(request):
     return Response({"message": "Greetings from your backend (   )(   ) !"})
 
+
+class TrickDefinitionList(generics.ListCreateAPIView):
+    queryset = TrickDefinition.objects.all()
+    serializer_class = TrickDefinitionSerializer
+
+class TrickDefinitionDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset=TrickDefinition.objects.all()
+    serializer_class = TrickDefinitionSerializer
+
+class RunList(generics.ListCreateAPIView):
+    queryset = Run.objects.all()
+    serializer_class = RunSerializer
 
 @api_view(["GET"])
 def get_trick_definitions(request):
@@ -54,42 +66,3 @@ def upload_run(request):
         )
     except Exception as e:
         return JsonResponse({"status": "error", "message": str(e)}, status=400)
-
-
-# @api_view(['GET', 'POST'])
-# @permission_classes([permissions.IsAuthenticated])
-# def weight_entries(request):
-#     if request.method == 'GET':
-#         entries = DailyWeight.objects.filter(user=request.user)
-#         serializer = WeightEntrySerializer(entries, many=True)
-#         return Response(serializer.data)
-
-#     elif request.method == 'POST':
-#         serializer = WeightEntrySerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save(user=request.user)  # Set the user to the current user
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-# @api_view(['GET', 'PUT', 'DELETE'])
-# @permission_classes([permissions.IsAuthenticated])
-# def weight_entry_detail(request, pk) -> Response | None:
-#     try:
-#         entry = DailyWeight.objects.get(pk=pk, user=request.user)
-#     except DailyWeight.DoesNotExist:
-#         return Response(status=status.HTTP_404_NOT_FOUND)
-
-#     if request.method == 'GET':
-#         serializer = WeightEntrySerializer(entry)
-#         return Response(serializer.data)
-
-#     elif request.method == 'PUT':
-#         serializer = WeightEntrySerializer(entry, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-#     elif request.method == 'DELETE':
-#         entry.delete()
-# return Response(status=status.HTTP_204_NO_CONTENT)
