1eb974662c71afe4d899a8c06e49b63ecb3050c2
COsta <kostyalasiy@gmail.com>
2024-04-28 08:53:06 -0600

    added run serializer

--- myapi/serializers.py
+++ myapi/serializers.py
@@ -1,16 +1,14 @@
-
 from rest_framework import serializers
-from .models import TrickDefinition
+from .models import TrickDefinition, Run
+
 
 class TrickDefinitionSerializer(serializers.ModelSerializer):
     class Meta:
         model = TrickDefinition
-        fields = '__all__'
+        fields = "__all__"
 
-# from .models import DailyWeight
 
-# class WeightEntrySerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = DailyWeight
-#         fields = ['id', 'date', 'weight']
-#         read_only_fields = ['id']
+class RunSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Run
+        fields = "__all__"
