bba191b10ee4156b097fd43451f24bb23189656d
COsta <kostyalasiy@gmail.com>
2024-05-03 05:02:37 -0500

    added short serializer

--- myapi/serializers.py
+++ myapi/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import TrickDefinition, Run
+from .models import TrickDefinition, Run, TrickInstance
 
 
 class TrickDefinitionSerializer(serializers.ModelSerializer):
@@ -8,7 +8,23 @@ class TrickDefinitionSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+class TrickDefinitionShortSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TrickDefinition
+        fields = ["id", "name"]
+
+
+class TrickInstanceSerializer(serializers.ModelSerializer):
+    trick_definition = TrickDefinitionShortSerializer(read_only=True)
+
+    class Meta:
+        model = TrickInstance
+        fields = ["id", "successful", "right", "reverse", "twisted", "trick_definition"]
+
+
 class RunSerializer(serializers.ModelSerializer):
+    tricks = TrickInstanceSerializer(many=True)
+
     class Meta:
         model = Run
-        fields = "__all__"
+        fields = ["id", "user", "date", "time", "wing", "site", "tricks"]
