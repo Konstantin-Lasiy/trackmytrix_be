1ff3dd77a34ae2aa08734eb27f70aa556b0447f0
COsta <kostyalasiy@gmail.com>
2024-05-10 20:52:30 +0300

    set cookie to 15 days

--- backend/settings.py
+++ backend/settings.py
@@ -172,7 +172,7 @@ SESSION_COOKIE_SECURE = True
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=15),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
